50663b896fc673179779b22a0ec9867517d84bda
Reinaldo Couto <43384617+Reinaldocouto@users.noreply.github.com>
2023-04-08 11:54:01 -0300

    first commit

---
+++ src/routes.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const routes = express.Router()
+
+const views = __dirname + "/views/"
+
+// req, res 
+routes.get('/', (req, res) => res.render(views + "index"))
+routes.get('/job', (req, res) => res.render(views + "job"))
+routes.get('/job/edit', (req, res) => res.render(views + "job-edit"))
+routes.get('/profile', (req, res) => res.render(views + "profile"))
+
+
+module.exports = routes;
+
